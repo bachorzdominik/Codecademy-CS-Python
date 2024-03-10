a0632ddb801dcfc5d1bcd0d886536f8ab12743a2
Dominik Bachórz <63674185+bachorzdominik@users.noreply.github.com>
2024-03-10 16:30:28 +0100

    Codecademy CS: Project: Who wants to be a millionaire: Improvments in questions class, __init__, get categories of questions & choose the category

--- projects/who-wants-to-be-a-millionaire/main.py
+++ projects/who-wants-to-be-a-millionaire/main.py
@@ -1,23 +1,45 @@
 import requests
 import json
+import sys
+import random
 
 
 class Questions:
-    def raw_data(self):
-        parameters = {
-            'amount': 10,
-            'type': 'multiple'
+    def __init__(self, amount=5, difficulty='easy', type='multiple', category=None):
+        self.parameters = {
+            'amount': amount,
+            'difficulty': difficulty,
+            'type': type,
+            'category': category
         }
-        response = requests.get('https://opentdb.com/api.php', params=parameters)
-        json_data = response.json()
-        responce_code = json_data['response_code']
-        questions_data = json_data['results']
-        
-        print(responce_code)
-        print(json.dumps(questions_data, indent=4))
-        
-        return questions_data
+        self.categories = self.get_categories()
+
+    def raw_request(self):
+        result = requests.get('https://opentdb.com/api.php', params=self.parameters)
+        print(result.url)
+        if result.status_code == 200:
+            return result
+        else:
+            raise Exception('Error: Unable to fetch data from the server')
+
+    def raw_data(self):
+        response = self.raw_request()
+        return response.json()['results']
     
+    def get_categories(self):
+        result = requests.get('https://opentdb.com/api_category.php')
+        if result.status_code == 200:
+            return result.json()['trivia_categories']
+        else:
+            raise Exception('Error: Unable to fetch data from the server')
+        
+    def choose_category(self):
+        print('Choose a category')
+        for i in self.categories:
+            print(f"{i['id']}. {i['name']}")
+        category = int(input('Enter the category number: '))
+        self.parameters['category'] = category
+        
     def shuffle_questions(self):
         pass
 
@@ -25,10 +47,11 @@ class Questions:
         pass
 
 
-class Quiz:
-    pass
-
-
-
-q = Questions()
-q.raw_data()
+easy_questions = Questions(category=None)
+easy_questions.choose_category()
+questions = easy_questions.raw_data()
+for i in questions:
+    print(i['question'])
+    print(i['correct_answer'])
+    print(i['incorrect_answers'])
+    print()
