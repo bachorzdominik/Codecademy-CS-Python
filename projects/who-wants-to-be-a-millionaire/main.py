f4cc0eff507c17069490359ae3bae4ddab7345bf
Dominik Bachórz <63674185+bachorzdominik@users.noreply.github.com>
2024-04-06 13:30:49 +0200

    Codecademy CS: Project: Who wants to be a millionaire: Quiz class implemented

--- projects/who-wants-to-be-a-millionaire/main.py
+++ projects/who-wants-to-be-a-millionaire/main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import sys
 import random
 import html
@@ -14,11 +13,12 @@ class Questions:
             'category': category
         }
         self.categories = self.get_categories()
-        self.raw_data = self.raw_data()
 
+    def fetch_data(self):
+        self.raw_data = self.raw_data()
+        
     def raw_request(self):
         result = requests.get('https://opentdb.com/api.php', params=self.parameters)
-        print(result.url)
         if result.status_code == 200:
             return result
         else:
@@ -51,14 +51,60 @@ class Questions:
 
             answers.insert(random.randint(0, len(answers)), correct_answer)
             
-            q_and_a_set['question'] = html.unescape(data['question']),
-            q_and_a_set['answers'] = [html.unescape(answer) for answer in answers],
+            q_and_a_set['question'] = html.unescape(data['question'])
+            q_and_a_set['answers'] = [html.unescape(answer) for answer in answers]
             q_and_a_set['correct_answer'] = html.unescape(data['correct_answer'])
 
             q_and_a_set_collection.append(q_and_a_set)
 
         return q_and_a_set_collection
 
-easy_questions = Questions(category=18)
-for q in easy_questions.create_question_set():
-    print(q)
+
+class Quiz:
+    def __init__(self, questions):
+        self.questions = questions
+        self.score = 0
+        self.total_questions = len(questions)
+        self.question_number = 0
+        self.incorrect_answers = 3
+    
+    def display_question(self):
+        print(f"Question {self.question_number + 1}: {self.questions[self.question_number]['question']}")
+        for i, answer in enumerate(self.questions[self.question_number]['answers']):
+            print(f"{i + 1}. {answer}")
+        user_answer = int(input('Enter the number of the correct answer: '))
+        self.check_answer(user_answer)
+        self.question_number += 1
+        if self.question_number < self.total_questions:
+            self.display_question()
+        else:
+            self.end_quiz()
+
+
+    def check_answer(self, user_answer):
+        if self.questions[self.question_number]['answers'][user_answer - 1] == self.questions[self.question_number]['correct_answer']:
+            print('Correct!')
+            self.score += 1
+        else:
+            print(f"Wrong! The correct answer is {self.questions[self.question_number]['correct_answer']}")
+            self.incorrect_answers -= 1
+            if self.incorrect_answers == 0:
+                self.end_quiz()
+                sys.exit()
+            else:
+                print(f"You have {self.incorrect_answers} incorrect answers left\n")
+
+    def end_quiz(self):
+        print(f"Your final score is {self.score}/{self.total_questions}")
+
+
+def main():
+    questions = Questions()
+    questions.choose_category()
+    questions.fetch_data()
+    quiz = Quiz(questions.create_question_set())
+    quiz.display_question()
+
+
+if __name__ == "__main__":
+    main()
